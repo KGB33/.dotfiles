63e39677aacb3f0759d102f6cfb827438003677f
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2021-07-19 02:55:54 -0700

    added some LSPs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,3 +25,9 @@ vim.g.python3_host_prog = '~/.config/nvim/.venv/bin/python'
 -- Spelling
 vim.o.spell = true
 vim.o.spelllang = "en_us"
+
+-- LSP
+require'lspconfig'.pyright.setup{}  -- community/pyright
+require'lspconfig'.bashls.setup{}   -- community/bash-language-server
+require'lspconfig'.dockerls.setup{} -- npm install -g dockerfile-language-server-nodejs
+require'lspconfig'.gopls.setup{}    -- community/gopls 
