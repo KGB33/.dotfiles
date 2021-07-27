ec9ce54eb6ffd6a5eaf1388b0c8c2283a53d8c58
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2021-07-27 08:25:13 -0700

    Disabled python2, ruby, & perl providers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.termguicolors=true
 vim.o.laststatus=2 -- Status line is Always on
+vim.o.completeopt = "menuone,noselect" -- Required for hrsh7th/nvim-compe
 --vim.o.python_highlight_all=1
 vim.cmd "syntax on"
 
@@ -31,3 +32,8 @@ require'lspconfig'.pyright.setup{}  -- community/pyright
 require'lspconfig'.bashls.setup{}   -- community/bash-language-server
 require'lspconfig'.dockerls.setup{} -- npm install -g dockerfile-language-server-nodejs
 require'lspconfig'.gopls.setup{}    -- community/gopls 
+
+-- Explicitly disable the providers in the health#providers#check
+vim.g.loaded_python_provider = 0 -- Python 2
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
