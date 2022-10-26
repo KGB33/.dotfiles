f9063506ce14d9b6318ace20b6209898433400b7
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-10-26 09:48:50 -0700

    Testing verified commit.
    Signed-off-by: Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,7 +13,7 @@ return require("packer").startup(function()
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 
-	-- Python Autoformatting
+	-- Python Auto-formatting
 	use("psf/black")
 
 	-- Treesitter
