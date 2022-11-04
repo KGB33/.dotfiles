b2c964be7bb84db21907a75722eda7a6d3f10383
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-11-04 09:05:23 -0700

    Minor changes & bug fixes.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,7 +41,7 @@ for _, lsp in ipairs(servers) do
 --			client.resolved_capabilities.document_formatting = false
 --			client.resolved_capabilities.document_range_formatting = false
 --		end,
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities()
 	})
 end
 
