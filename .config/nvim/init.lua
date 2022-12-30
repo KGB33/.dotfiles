595f23509b252b8b680f6cf309d44b45e125297e
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-12-30 13:18:38 -0800

    feat(plugins): Added Mind.nvim
    Added mind.nvim plugin.
    Formatted plugin file.
    Modified tab fuctionality.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,13 +14,13 @@ require("keybinds")
 -- Themes
 vim.cmd([[ colorscheme gruvbox]])
 vim.o.laststatus = 3
+vim.o.signcolumn = "yes"
 
 -- LSP
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.setup()
-
 
 -- Langage Providers
 vim.g.python3_host_prog = "~/.config/nvim/.venv/bin/python"
@@ -41,4 +41,7 @@ vim.o.relativenumber = true
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
