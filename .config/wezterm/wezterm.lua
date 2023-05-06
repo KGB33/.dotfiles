7c981233b73ef57906990eab5eb13f9b63a4dfd2
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-05-06 12:56:15 -0700

    chore(fontname): Nerd fonts were moved from AUR
    New repos new names - NF v3.0.0 might have caused the
    new names too.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+return {
+    color_scheme = "Gruvbox dark, hard (base16)",
+    window_background_opacity = 0.66,
+    font = wezterm.font 'FiraCode Nerd Font',
+    font_size = 10,
+
+    enable_wayland = true,
+}
