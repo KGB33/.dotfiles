c1f88df361be2298a17057efad719a76bbe13e10
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-03-16 14:18:01 -0700

    Update dagger sdk directory.

--- fish/functions/dagvenv.fish
+++ fish/functions/dagvenv.fish
@@ -2,9 +2,9 @@ if test ! -e dagger.json
   echo "Dagger.json not found..."
   return 1
 end
-if test ! -e ./sdk
-  echo "Generated SDK (./sdk) not found..."
+if test ! -e ./dagger/sdk
+  echo "Generated SDK (./dagger/sdk) not found..."
   return 1
 end
 venv
-python -m pip install -e ./sdk
+python -m pip install -e ./dagger/sdk
