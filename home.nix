2a1325963ddc52479aefc327f35a5cda7eae92d5
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-04-02 09:48:00 -0700

    Add River WM.

--- home.nix
+++ home.nix
@@ -73,6 +73,70 @@
     # settings = {};
   };
 
+  wayland.windowManager.river = {
+    enable = true;
+    settings = {
+      declare-mode = [ "Display" ];
+      map = {
+        normal = {
+          # Spawns
+          "Super R" = "spawn fuzzel";
+          # Meta
+          "Super M" = "exit";
+          "Super D" = "enter-mode Display";
+
+          # Window Focus
+          "Super H" = "focus-view left";
+          "Super J" = "focus-view down";
+          "Super K" = "focus-view up";
+          "Super L" = "focus-view right";
+          "Super N" = "focus-view next";
+          "Super P" = "focus-view previous";
+
+          # Window Movement
+          "Super+Shift H" = "swap left";
+          "Super+Shift J" = "swap down";
+          "Super+Shift K" = "swap up";
+          "Super+Shift L" = "swap right";
+          "Super+Shift N" = "swap next";
+          "Super+Shift P" = "swap previous";
+          "Super Z" = "zoom";
+
+        } // (
+          # Tags
+          let
+            pow = exp:
+              if exp == 0 then 1
+              else 2 * pow (exp - 1);
+            str = builtins.toString;
+          in
+          builtins.listToAttrs (
+            builtins.genList (x: { name = "Super ${str x}"; value = "set-focused-tags ${str (pow x)}"; }) 9
+          )
+        );
+        Display = {
+          "None Escape" = "enter-mode normal";
+          # Move
+          "Super H" = "move left  100";
+          "Super J" = "move down  100";
+          "Super K" = "move up    100";
+          "Super L" = "move right 100";
+          # Snap
+          "Shift H" = "snap left";
+          "Shift J" = "snap down";
+          "Shift K" = "snap up";
+          "Shift L" = "snap right";
+          # Resize
+          "Super+Shift  H" = "resize horizontal -100";
+          "Super+Shift  J" = "resize vertical    100";
+          "Super+Shift  K" = "resize vertical   -100";
+          "Super+Shift  L" = "resize horizontal  100";
+        };
+      };
+      spawn = [ "rivertile" ];
+      default-layout = "rivertile";
+    };
+  };
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true; # For steam
@@ -263,7 +327,7 @@
     '';
     loginShellInit = ''
       if test (tty) = /dev/tty1
-        Hyprland
+        river
       end
     '';
 
@@ -350,6 +414,7 @@
       size = 10;
     };
     theme = "Everforest Dark Medium";
+    # enable_audio_bell = "no";
   };
 
   programs.tmux = {
