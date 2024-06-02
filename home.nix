c61ff1a5bed5ab6dedd0ee16651710384de83ac1
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-06-02 08:34:29 -0700

    Remove Hyprland

--- home.nix
+++ home.nix
@@ -22,9 +22,9 @@
     cabal-install
     ghc
     grim
-    hypridle
-    hyprlock
-    hyprpaper
+    # hypridle
+    # hyprlock
+    # hyprpaper
     nh
     slurp
     uv
@@ -152,68 +152,68 @@
     };
   };
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true; # For steam
-    settings = {
-      exec-once = [
-        "eww open top_bar"
-        "hyprpaper"
-      ];
-      monitor = [
-        "eDP-2,preferred,auto,1"
-        ",preferred,auto,1,mirror,eDP-2"
-      ];
-      decoration = {
-        rounding = 5;
-      };
-      "$mod" = "SUPER";
-      binde = [
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
+  # wayland.windowManager.hyprland = {
+  #   enable = true;
+  #   xwayland.enable = true; # For steam
+  #   settings = {
+  #     exec-once = [
+  #       "eww open top_bar"
+  #       "hyprpaper"
+  #     ];
+  #     monitor = [
+  #       "eDP-2,preferred,auto,1"
+  #       ",preferred,auto,1,mirror,eDP-2"
+  #     ];
+  #     decoration = {
+  #       rounding = 5;
+  #     };
+  #     "$mod" = "SUPER";
+  #     binde = [
+  #       ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+  #       ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
 
-        # ", XF86AudioNext,"
-        # ", XF86AudioPrev,"
+  #       # ", XF86AudioNext,"
+  #       # ", XF86AudioPrev,"
 
-        ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
-        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
-      ];
-      bind =
-        [
-          "$mod, M, exit"
-          "$mod, F, exec, firefox"
-          "$mod, Q, exec, kitty"
-          "$mod, R, exec, fuzzel"
-          "$mod, L, exec, hyprlock"
-          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-          '', Print, exec, grim -g "$(slurp -d)" - | wl-copy''
-          ''$mod, Print, exec, grim -g "$(slurp -d)"''
-          # ", XF86AudioPlay,"
-          # ", XF86RFKill, " # Airplane mode
-          # ", XF86Tools, " # Framework Icon
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-          builtins.concatLists (builtins.genList
-            (
-              x:
-              let
-                ws =
-                  let
-                    c = (x + 1) / 10;
-                  in
-                  builtins.toString (x + 1 - (c * 10));
-              in
-              [
-                "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-              ]
-            )
-            10)
-        );
-    };
-  };
+  #       ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
+  #       ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+  #     ];
+  #     bind =
+  #       [
+  #         "$mod, M, exit"
+  #         "$mod, F, exec, firefox"
+  #         "$mod, Q, exec, kitty"
+  #         "$mod, R, exec, fuzzel"
+  #         "$mod, L, exec, hyprlock"
+  #         ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+  #         '', Print, exec, grim -g "$(slurp -d)" - | wl-copy''
+  #         ''$mod, Print, exec, grim -g "$(slurp -d)"''
+  #         # ", XF86AudioPlay,"
+  #         # ", XF86RFKill, " # Airplane mode
+  #         # ", XF86Tools, " # Framework Icon
+  #       ]
+  #       ++ (
+  #         # workspaces
+  #         # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+  #         builtins.concatLists (builtins.genList
+  #           (
+  #             x:
+  #             let
+  #               ws =
+  #                 let
+  #                   c = (x + 1) / 10;
+  #                 in
+  #                 builtins.toString (x + 1 - (c * 10));
+  #             in
+  #             [
+  #               "$mod, ${ws}, workspace, ${toString (x + 1)}"
+  #               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+  #             ]
+  #           )
+  #           10)
+  #       );
+  #   };
+  # };
 
   programs.eww = {
     enable = true;
