cba98089037c4a53c96f6d03f128d18dde7226bf
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-10-28 07:39:29 -0700

    git: Change primary git email to be the default.

--- home.nix
+++ home.nix
@@ -126,7 +126,7 @@
 
   programs.git = {
     enable = true;
-    userEmail = "keltonbassingthwaite@gmail.com";
+    userEmail = lib.mkOptionDefault "keltonbassingthwaite@gmail.com";
     userName = "Kelton Bassingthwaite";
 
     lfs = {
