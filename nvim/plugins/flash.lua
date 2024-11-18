a6219299dfeda095e6c2b90d37eee12c17c7c8a1
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-11-17 20:53:17 -0800

    Use flash.nvim on search by default.

--- nvim/plugins/flash.lua
+++ nvim/plugins/flash.lua
@@ -1,6 +1,8 @@
 local flash = require("flash");
 
-flash.toggle(true);
+flash.setup({
+    modes = { search = { enabled = true } }
+})
 
 vim.keymap.set('n', '<leader>fj', function() flash.jump() end, { desc = '[F]lash [J]upm' })
 vim.keymap.set('n', '<leader>fs', function() flash.treesitter_search() end, { desc = '[F]lash treesitter [S]earch' })
