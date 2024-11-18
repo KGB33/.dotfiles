961b50965612f5512338aec121fd65550b4fbe9f
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-11-17 21:10:25 -0800

    Don't use flash search by default.
    It only searches the visible part of the buffer.

--- nvim/plugins/flash.lua
+++ nvim/plugins/flash.lua
@@ -1,9 +1,5 @@
 local flash = require("flash");
 
-flash.setup({
-    modes = { search = { enabled = true } }
-})
-
 vim.keymap.set('n', '<leader>fj', function() flash.jump() end, { desc = '[F]lash [J]upm' })
 vim.keymap.set('n', '<leader>fs', function() flash.treesitter_search() end, { desc = '[F]lash treesitter [S]earch' })
 vim.keymap.set('n', '<leader>ft', function() flash.treesitter() end, { desc = '[F]lash [T]reesitter' })
