9e29075d51d1ac1ec219cf1e120598da34323bc1
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-05-01 13:56:23 -0700

    chore(nvim): Update Avante model

--- nvim/plugins/avante.lua
+++ nvim/plugins/avante.lua
@@ -1,20 +1,13 @@
 -- https://github.com/yetone/avante.nvim
 require('avante_lib').load()
 
--- Ollama API Documentation https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
-local role_map = {
-    user = "user",
-    assistant = "assistant",
-    system = "system",
-    tool = "tool",
-}
-
 require('avante').setup({
     auto_suggestions_provider = "ollama",
     debug = true,
     provider = "ollama",
     ollama = {
         -- model = "qwen2.5-coder:7b",
-        model = "gemma3:12b",
+        -- model = "gemma3:12b",
+        model = "qwen3:30b",
     },
 })
