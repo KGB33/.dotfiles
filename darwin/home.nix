fd94fb7d678e4c8178275684663c438b79424d4d
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-05-01 15:48:19 -0700

    feat(darwin): Add Aerospace config

--- darwin/home.nix
+++ darwin/home.nix
@@ -40,6 +40,71 @@
     enable = true;
     userSettings = {
       start-at-login = true;
+      mode = {
+        main.binding = let
+          workspaces = [
+            "0"
+            "1"
+            "2"
+            "3"
+            "4"
+            "5"
+            "6"
+            "7"
+            "8"
+            "9"
+            "a"
+            "b"
+            "c"
+            "d"
+            "e"
+            "f"
+            "g"
+            "h"
+            "i"
+            "j"
+            "k"
+            "l"
+            "m"
+            "n"
+            "o"
+            "p"
+            "q"
+            "r"
+            "s"
+            "t"
+            "u"
+            "v"
+            "w"
+            "x"
+            "y"
+            "z"
+          ];
+          mappings = builtins.concatLists (map (c: [
+              {
+                name = "alt-${c}";
+                value = "workspace ${c}";
+              }
+              {
+                name = "alt-shift-${c}";
+                value = "move-node-to-workspace ${c}";
+              }
+            ])
+            workspaces);
+        in
+          builtins.listToAttrs mappings
+          // {
+            alt-tab = "workspace-back-and-forth";
+            alt-shift-tab = "move-workspace-to-monitor --warp-around next";
+            alt-shift-semicolon = "mode service";
+          };
+        service.binding = {
+          esc = ["reload config" "mode main"];
+          r = ["flatten-workspace-tree" "mode main"];
+          f = ["layout floating tiling" "mode main"];
+          backspace = ["close-all-windows-but-current" "mode main"];
+        };
+      };
     };
   };
 }
