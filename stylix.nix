93ed86f178ec1381e9b1a6a7693b69cb03b2c6c1
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-06-10 08:40:56 -0700

    Sylix: autoenable again...

--- stylix.nix
+++ stylix.nix
@@ -1,29 +1,9 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
-    autoEnable = false;
+    autoEnable = true;
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-light-hard.yaml";
-    targets = {
-      neovim.enable = true;
-      wezterm.enable = true;
-      hyprland.enable = true;
-      hyprpaper.enable = true;
-      hyprlock.enable = true;
-      bat.enable = true;
-      bemenu.enable = true;
-      btop.enable = true;
-      nixcord.enable = true;
-      fuzzel. enable = true;
-      k9s.enable = true;
-      lazygit.enable = true;
-      nushell.enable = true;
-      qt.enable = true;
-      starship.enable = true;
-      tmux.enable = true;
-      waybar.enable = true;
-      yazi.enable = true;
-    };
     fonts = {
       monospace = {
         package = pkgs.fira-code;
