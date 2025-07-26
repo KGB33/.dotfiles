715ca4fa407bed892663a92ea059ac6dbaf048f7
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-07-25 22:24:04 -0700

    feat: Add television

--- apps/television.nix
+++ apps/television.nix
@@ -1,17 +1,78 @@
-{tv, ...}: {
+{
+  lib,
+  config,
+  pkgs,
+  tv,
+  ...
+}: let
+  tvOn = config.programs.television.enable;
+in {
   programs.television = {
     package = tv;
     enable = true;
-    channels = {
-      custom = {
-        tmux = [
-          {
-            name = "session select";
-            source_command = "tmux ls -F '#{session_name}'";
-            preview_command = "tmux ls -F'#{session_id}#{session_name}: #{T:tree_mode_format}'";
-          }
-        ];
+    settings = {
+      tick_rate = 60;
+      default_channel = "meta";
+      ui = {
+        use_nerd_font_icons = true;
+      };
+      keybindings = {
+        "}" = "scroll_preview_half_page_down";
+        "{" = "scroll_preview_half_page_up";
+      };
+      shell_integration = {
+        channel_triggers = {
+          "meta" = ["tv"];
+          "alias" = ["alias" "unalias"];
+          "env" = ["export" "unset"];
+          "dirs" = ["cd" "ls" "rmdir"];
+          "git-diff" = ["git add" "git restore"];
+          "git-branch" = [
+            "git checkout"
+            "git switch"
+            "git branch"
+            "git merge"
+            "git rebase"
+            "git pull"
+            "git push"
+          ];
+          "git-log" = ["git log" "git show"];
+          "docker-images" = ["docker run"];
+          "git-repos" = ["git clone"];
+        };
+        keybindings = {
+          smart_autocomplete = "ctrl-t";
+        };
       };
     };
   };
+
+  home.file = let
+    prefix = ".config/television/cable";
+    formatter = pkgs.formats.toml {};
+  in {
+    "${prefix}/tss.toml" = {
+      enable = tvOn;
+      source = formatter.generate "tss.toml" {
+        metadata = {name = "tss";};
+        source = {command = "tmux ls -F '#{session_name}'";};
+      };
+    };
+    "${prefix}/meta.toml" = {
+      enable = tvOn;
+      source = formatter.generate "meta.toml" {
+        metadata = {name = "meta";};
+        source = {
+          command = "tv list-channels";
+        };
+      };
+    };
+  };
+  home.activation =
+    lib.mkIf tvOn
+    {
+      televisionSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        run ${tv}/bin/tv update-channels
+      '';
+    };
 }
--- apps/tmux.nix
+++ apps/tmux.nix
@@ -1,12 +1,21 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.tmux = {
     enable = true;
     escapeTime = 300;
     terminal = "tmux-256color";
-    extraConfig = ''
+    extraConfig = let
+      tss =
+        if config.programs.television.enable
+        then "tv tss"
+        else "tss";
+    in ''
       set-option -sa terminal-features 'xterm-256color:RGB'
       bind C-g display-popup -E -w 85% -h 85% "${pkgs.lazygit}/bin/lazygit"
-      bind t display-popup -E -w 85% -h 85% "tss"
+      bind t display-popup -E -w 85% -h 85% "${tss}"
     '';
   };
 }
