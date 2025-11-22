d0c7700ef9095b279facf89bee2b21e2f61e7899
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-11-21 18:09:34 -0800

    helm network update

--- systems/helm/network.nix
+++ systems/helm/network.nix
@@ -5,4 +5,11 @@
       allowedTCPPorts = [];
     };
   };
+
+  systemd.network = {
+    networks."05-eth0" = {
+      matchConfig.Name = "enp74s0";
+      networkConfig.DHCP = "yes";
+    };
+  };
 }
