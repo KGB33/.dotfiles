e313e57de9e63cde8fcc027d74a3ea09cc827556
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-01-25 16:46:46 -0800

    chore(system): Patch kernal for dagger

--- systems/geppetto/configuration.nix
+++ systems/geppetto/configuration.nix
@@ -10,6 +10,17 @@
   ];
 
   boot.initrd.kernelModules = ["amdgpu"];
+  boot.kernelPatches = [
+    # TODO: Remove this when dagger fixes their stuff
+    # https://github.com/dagger/dagger/issues/11694
+    {
+      name = "netfilter-xtables-legacy";
+      patch = null;
+      extraConfig = ''
+        NETFILTER_XTABLES_LEGACY y
+      '';
+    }
+  ];
   hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
   ];
