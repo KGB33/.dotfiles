968f2edb890fadd7ac43b5e485553fe9dc92dd81
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-05-08 20:22:22 +0000

    feat: NuShell

---
+++ modules/aspects/apps/nushell.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  apps,
+  den,
+  ...
+}:
+{
+  apps.nushell-login = {
+    includes =
+      let
+        unused = den.lib.take.unused __findFile;
+        __findFile = unused den.lib.__findFile;
+      in
+      [
+        apps.nushell
+        (<den/user-shell> "bash")
+      ];
+    nixos =
+      { pkgs, ... }:
+      {
+        environment.shells = [ pkgs.nushell ];
+        programs.bash.interactiveShellInit = ''
+          if ! [ "$TERM" = "dumb" ] && [ -z "$BASH_EXECUTION_STRING" ]; then
+            exec nu
+          fi
+        '';
+      };
+  };
+  apps.nushell = {
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.shell.enableNushellIntegration = true;
+        programs.nushell = {
+          enable = true;
+          plugins =
+            pkgs.nushellPlugins
+            |> lib.filterAttrs (name: value: lib.isDerivation value)
+            |> lib.filterAttrs (name: value: !(value.meta.broken or false))
+            |> lib.filterAttrs (
+              name: value:
+              !(lib.elem name [
+                "bson"
+                "highlight"
+              ])
+            )
+            |> lib.attrValues;
+        };
+      };
+  };
+}
