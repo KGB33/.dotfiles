be00d9509c927f6e444e83a674b2c8fd8173e664
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-05-08 18:02:58 +0000

    nix: enable experimental features

--- modules/aspects/geppetto.nix
+++ modules/aspects/geppetto.nix
@@ -6,6 +6,18 @@
     # NixOS configuration for geppetto.
     nixos = {pkgs, ...}: {
       environment.systemPackages = [pkgs.hello];
+        nix.settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "pipe-operators"
+          ];
+        };
+
+        boot.loader.systemd-boot = {
+          enable = true;
+          configurationLimit = 16;
+        };
     };
 
     # <host>.provides.<user>, via den.provides.mutual-provider
