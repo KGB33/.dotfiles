38f60d5fe0d721ef62e5b9d69f5790829b7e7aed
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-05-09 17:46:35 +0000

    add stylix

--- flake.nix
+++ flake.nix
@@ -28,5 +28,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
