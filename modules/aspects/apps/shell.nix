2b4761e3eb44b37116c0ceedb557d7adf76e3930
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-05-10 04:41:17 +0000

    add atuin

--- modules/aspects/apps/shell.nix
+++ modules/aspects/apps/shell.nix
@@ -1,6 +1,32 @@
-{ ... }:
+{ lib, ... }:
 {
   apps.shell.homeManager = {
+
+    programs.atuin = {
+      enable = true;
+      daemon.enable = true;
+    };
+
+    programs.nushell = {
+      configFile.text =
+        lib.mkBefore
+          # nushell
+          ''
+            if (is-terminal --stdin) and (is-terminal --stdout) {
+              let tmux_current = ($env.TMUX? | default "")
+              let tmux_previous = ($env.ATUIN_HEX_TMUX? | default "")
+
+              if ($env.ATUIN_HEX_ACTIVE? | default "" | is-empty) or ($tmux_current != $tmux_previous) {
+                $env.ATUIN_HEX_ACTIVE = "1"
+                $env.ATUIN_HEX_TMUX = $tmux_current
+                exec atuin hex
+              }
+            }
+
+          '';
+
+    };
+
     programs.starship = {
       enable = true;
       presets = [
