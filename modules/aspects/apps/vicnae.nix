651b1b9f0cc38d9a469d32f23f6b0e7878660e3b
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-05-13 01:42:11 +0000

    add vicinae cache

--- modules/aspects/apps/vicnae.nix
+++ modules/aspects/apps/vicnae.nix
@@ -11,6 +11,14 @@
   };
 
   apps.vicinae = {
+    nixos =
+      { ... }:
+      {
+        nix.settings = {
+          extra-substituters = [ "https://vicinae.cachix.org" ];
+          extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+        };
+      };
     homeManager =
       { pkgs, ... }:
       {
