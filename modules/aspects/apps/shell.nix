4b517167d042c20d4fd58cbae74a4ab69f63e5c3
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-06-10 08:49:57 -0700

    shell: Remove Atuin pty/hex

--- modules/aspects/apps/shell.nix
+++ modules/aspects/apps/shell.nix
@@ -12,26 +12,6 @@
       nix-direnv.enable = true;
     };
 
-    programs.nushell = {
-      configFile.text =
-        lib.mkBefore
-          # nushell
-          ''
-            if (is-terminal --stdin) and (is-terminal --stdout) {
-              let tmux_current = ($env.TMUX? | default "")
-              let tmux_previous = ($env.ATUIN_HEX_TMUX? | default "")
-
-              if ($env.ATUIN_HEX_ACTIVE? | default "" | is-empty) or ($tmux_current != $tmux_previous) {
-                $env.ATUIN_HEX_ACTIVE = "1"
-                $env.ATUIN_HEX_TMUX = $tmux_current
-                exec atuin hex
-              }
-            }
-
-          '';
-
-    };
-
     programs.starship = {
       enable = true;
       presets = [
