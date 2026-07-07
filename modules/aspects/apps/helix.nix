28e0298276be6d2f0acc9d841cf790aa35c96c58
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2026-07-07 15:15:58 -0700

    helix: Install steel lsp

--- modules/aspects/apps/helix.nix
+++ modules/aspects/apps/helix.nix
@@ -1,13 +1,16 @@
 { ... }:
 {
-  apps.helix.homeManager = { pkgs, ... }: {
+  apps.helix.homeManager = { pkgs, config, ... }: {
     programs.helix = {
       package = pkgs.steelix;
       enable = true;
+      extraPackages = with pkgs; [ steel-language-server ];
     };
     xdg.configFile = {
       "helix/helix.scm".source = ./helix/helix.scm;
       "helix/init.scm".source = ./helix/init.scm;
     };
+
+    home.sessionVariables."STEEL_LSP_HOME" = "${config.xdg.configHome}/steel_lsp/";
   };
 }
--- modules/aspects/apps/helix/init.scm
+++ modules/aspects/apps/helix/init.scm
@@ -1,3 +1,8 @@
 (require-builtin steel/random as rand::)
 (require (prefix-in helix. "helix/commands.scm"))
 (require (prefix-in helix.static. "helix/static.scm"))
+
+(require "helix/configuration.scm")
+(define-lsp "steel-language-server" (command "steel-language-server") (args '()))
+(define-language "scheme"
+                 (language-servers '("steel-language-server")))
